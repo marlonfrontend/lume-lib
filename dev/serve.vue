bf39eada11c9caeb653270e7066717d48bddbc99
marlon.santos <marlon.santos@tuibrasil.com>
2022-03-10 18:35:23 -0300

    init

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { LumeSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  LumeSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <lume-sample />
+  </div>
+</template>
